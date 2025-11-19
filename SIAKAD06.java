e145530430d2dfa480d1b4899dd88c0d592efa1c
Ayla Fazi <nisafazilatunalya@gmail.com>
2025-11-19 22:41:34 +0700

    percobaan 4 no 8

--- SIAKAD06.java
+++ SIAKAD06.java
@@ -6,14 +6,26 @@ public class SIAKAD06 {
 
         for (int i = 0; i < nilai.length; i++) {
         System.out.println("Input nilai mahasiswa ke-" + (i + 1));
-       
+         double totalPerSiswa = 0;
 
         for (int j = 0; j < nilai[i].length; j++) {
         System.out.print("Nilai mata kuliah " + (j+1)+": "); 
         nilai[i][j] = sc.nextInt(); 
-       
+        totalPerSiswa += nilai[i][j];
         }
-        
+         System.out.println("Nilai rata-rata: " + totalPerSiswa/3);
+}
+System.out.println("\n=================================");
+System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
+
+for (int j=0; j < 3; j++) {
+double totalPerMatkul = 0;
+
+for (int i=0; i<4; i++) { 
+totalPerMatkul += nilai[i][j];
+}
+
+System.out.println("Mata Kuliah" +  (j+1)+":" + totalPerMatkul / 4);
 }
     }
 }
